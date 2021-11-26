2c69f2cf181fb14fa552b458f4a4370175a1f935
tranhiepnguyenhuy1999 <tranhiepnguyenhuy1999@gmail.com>
2021-11-26 21:49:31 +0700

    update add ShipE Class

---
+++ 04-Collision/ShipE.cpp
@@ -0,0 +1,54 @@
+#include "ShipE.h"
+
+void ShipE::GetBoundingBox(float &left, float &top, float &right, float &bottom)
+{
+	left = x;
+	top = y;
+	right = x + GOOMBA_BBOX_WIDTH;
+
+	if (state == BATTRAP_STATE_DIE)
+		bottom = y + GOOMBA_BBOX_HEIGHT_DIE;
+	else
+		bottom = y + GOOMBA_BBOX_HEIGHT;
+}
+
+void ShipE::Update(DWORD dt, vector<LPGAMEOBJECT> *coObjects)
+{
+	//
+}
+
+void ShipE::Render()
+{
+	int ani = BATTRAP_ANI_STATIC;
+
+	switch (state)
+	{
+	case BATTRAP_STATE_OPEN:
+		ani = BATTRAP_ANI_OPEN;
+
+	case BATTRAP_STATE_DIE:
+		ani = BATTRAP_ANI_DIE;
+	default:
+		break;
+	}
+
+	animations[ani]->Render(x, y);
+	//RenderBoundingBox();
+}
+
+void ShipE::SetState(int state)
+{
+	CGameObject::SetState(state);
+
+	switch (state)
+	{
+	case BATTRAP_STATE_DIE:
+		y += GOOMBA_BBOX_HEIGHT - GOOMBA_BBOX_HEIGHT_DIE + 1;
+		vx = 0;
+		vy = 0;
+		break;
+	case BATTRAP_STATE_OPEN:
+		vx = -GOOMBA_WALKING_SPEED;
+	}
+}
+
